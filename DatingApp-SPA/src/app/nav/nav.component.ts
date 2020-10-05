c012fbd18eec76d5396ab9394f0392141ee3d604
S-Shami <sagaciousshami325@gmail.com>
2020-10-05 23:21:33 +0500

    Error handling in API and Angular interceptor

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('Login Successfull');
     }, error => {
-      console.error('Failed to Login');
+      console.error(error);
     });
   }
 
